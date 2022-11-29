7a7deabb942f7f392e366b97cf55b1818ec1dcfe
unknown <rafaelgsaravia@gmail.com>
2022-11-29 16:00:55 -0300

    3er entrega

---
+++ stock.js
@@ -0,0 +1,7 @@
+let stockProductos = [
+    {id: 1, nombre: "Calzas", tipo: "Calza", cantidad: 1, desc: "Calzas de mujer", precio: 1200, talle: "L", img: './imagenes/calsas.jpg'},
+    {id: 2, nombre: "Calzas", tipo: "Calza Ciclista", cantidad: 1, desc: "Calzas para Ciclista", precio: 1100, talle: "L", img: './imagenes/calsaciclista.jpg'},
+    {id: 3, nombre: "Pantalon", tipo: "Jogging", cantidad: 1, desc: "Pantalon jogging", precio: 1200, talle: "M", img: './imagenes/pantalon.jpg'},
+    {id: 4, nombre: "Remeras", tipo: "Sudadera", cantidad: 1, desc: "Sudadera para mujer", precio: 1400, talle: "M", img: './imagenes/sudadera.jpg'},
+
+]
